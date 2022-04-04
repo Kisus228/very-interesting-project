4b9668f16bcab329b967718a0e6f8d0934b33d48
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-04 12:27:29 +0500

    Add Routes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,27 +1,29 @@
 // @ts-ignore
-import styles from './App.less';
+import classes from './App.less';
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
+import { Routes, Route } from 'react-router-dom';
+import Search from "./components/Search/Search";
+import Liked from "./components/Liked/Liked";
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={classes.AppWrapper}>
+            <Header/>
+            <main className={classes.AppContentWrapper}>
+                <Navigation/>
+                <div className={classes.AppContentContainer}>
+                    <Routes>
+                        <Route path="/">
+                            <Route index element={<Search />} />
+                            <Route path="liked" element={<Liked />}/>
+                        </Route>
+                    </Routes>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
