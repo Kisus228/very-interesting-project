4d59a96e05e33a3cc639a090a7a54ffeea587038
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-04 12:25:06 +0500

    Add pages

---
+++ react-app/src/components/Liked/Liked.tsx
@@ -0,0 +1,13 @@
+// @ts-ignore
+import classes from './Liked.less';
+import React from 'react';
+
+const Liked = () => {
+    return (
+        <div>
+            2
+        </div>
+    );
+};
+
+export default Liked;
---
+++ react-app/src/components/Search/Search.tsx
@@ -0,0 +1,13 @@
+// @ts-ignore
+import classes from './Search.less';
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            1
+        </div>
+    );
+};
+
+export default Search;
