2aa27222024fb262d3430cf107945a137187dd37
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-10 01:53:56 +0500

    End working on page Search

--- react-app/src/components/Search/Search.tsx
+++ react-app/src/components/Search/Search.tsx
@@ -1,8 +1,9 @@
-// @ts-ignore
 import classes from './Search.less';
 import React, {useState} from 'react';
 import Filter from "../Filter/Filter";
 import avatar from "./../../assets/avatar.png";
+import Button from "../Common/FormControl/Button";
+import LikeButton from "../Common/FormControl/LikeButton";
 
 type StateType = {
     id: number,
@@ -49,10 +50,10 @@ const Search = () => {
     return (
         <div className={classes.SearchWrapper}>
             <div>
-                <Filter />
+                <Filter/>
             </div>
             <div>
-                <h2>Найдено по запросу: 67 человек</h2>
+                <h2>Найдено людей по запросу: {state.length}</h2>
                 <ul className={classes.ProfileItemsWrapper}>
                     {
                         state.map(item => <ProfileItem key={item.id} {...item}/>)
@@ -64,18 +65,23 @@ const Search = () => {
 };
 
 const ProfileItem = (props: StateType) => {
+    const [liked, setLiked] = useState(false);
+
     return (
         <li className={classes.ProfileItem}>
             <div className={classes.ProfileAvatar}>
-                <img width={100} height={100} src={avatar} alt="avatar"/>
+                <img width={125} height={125} src={avatar} alt="avatar"/>
             </div>
             <div className={classes.ProfileInfo}>
                 <h3 className={classes.ProfileName}>{props.name}</h3>
                 <p>Специальность: {props.speciality}</p>
                 <p>Стаж: {props.experience} лет</p>
-                <p>Навыки: {props.competence}</p>
+                <p>Навыки: {props.competence.join(", ")}</p>
             </div>
-            <div>1</div>
+            <div className={classes.ProfileAction}>
+                <LikeButton liked={liked} onClick={() => setLiked(!liked)}/>
+                <Button>Посмотреть профиль</Button>
+            </div>
         </li>
     );
 }
