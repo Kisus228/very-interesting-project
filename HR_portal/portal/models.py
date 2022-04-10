ba11fa4a6d074917c7064d98e2499907d39e4e81
Сухраб <Sukhrab2001@mail.ru>
2022-04-10 11:03:41 +0500

    кобан

--- HR_portal/portal/models.py
+++ HR_portal/portal/models.py
@@ -65,3 +65,39 @@ class Vacancy(models.Model):
         return self.name
 
 
+class Worker(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Работник', blank=True)
+    birthdate = models.DateField(verbose_name='Дата рождения', blank=True)
+    liked_apps = models.JSONField(verbose_name='Понравившиеся заявки', blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
+class JobApplications(models.Model):
+    vacancy_id = models.ForeignKey(Vacancy, on_delete=models.CASCADE, verbose_name='ID вакансии')
+    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE, verbose_name='ID работника')
+
+    class Meta:
+        verbose_name = 'Заявка на вакансию'
+        verbose_name_plural = 'Заявки на вакансии'
+
+    def __str__(self):
+        return self.vacancy_id
+
+
+class Resume(models.Model):
+    resume_id = models.IntegerField(verbose_name='ID резюме')
+    vk_link = models.TextField(verbose_name='Ссылка на ВК')
+    tg_link = models.TextField(verbose_name='Ссылка на Телеграм')
+    github_link = models.TextField(verbose_name='Ссылка на GitHub')
+    gitlab_link = models.TextField(verbose_name='Ссылка на GitLab')
+    resume_text = models.TextField(verbose_name='Текст резюме')
+    skills = models.JSONField(verbose_name='Список навыков')
+    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE, verbose_name='ID работника')
+
+    class Meta:
+        verbose_name = 'Резюме'
+
+    def __str__(self):
+        return self.resume_id
