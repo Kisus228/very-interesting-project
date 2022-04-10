02792eb311df3397bcc338d57e308eb578459a38
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-10 20:18:51 +0500

    Edited buttons

--- react-app/src/components/Common/FormControl/Button.tsx
+++ react-app/src/components/Common/FormControl/Button.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import classes from './Button.less';
 
 interface PropsType {
-
+    onClick?: () => void;
+    size?: "small" | "large";
+    color?: "red";
 }
 
 const Button: React.FC<PropsType> = (props) => {
+    const small = props.size === "small" && classes.small;
+    const large = props.size === "large" && classes.large;
+    const color = props.color === "red" && classes.redButton;
+    const style = `${classes.button} ${small} ${large} ${color}`;
+
     return (
-        <button className={classes.button}>{props.children}</button>
+        <button onClick={props.onClick} className={style}>{props.children}</button>
     );
 };
 
