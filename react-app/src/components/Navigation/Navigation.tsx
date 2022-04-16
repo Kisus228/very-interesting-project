65318f3354ba6765c154f667a3fc10ba6a9a52ec
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-16 16:32:09 +0500

    Refactored Navigation

--- react-app/src/components/Navigation/Navigation.tsx
+++ react-app/src/components/Navigation/Navigation.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import classes from './Navigation.less';
-import {NavLink, useLocation, useNavigate} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 
 const Navigation = () => {
     const navigate = useNavigate();
-    const url = useLocation().pathname;
-    const pathname = url.split('/')[1];
+
     return (
         <div className={classes.NavigationWrapper}>
             <ul className={classes.Navigation}>
                 <li>
-                    <NavLink to={'/liked'} className={pathname === 'liked' ? classes.active : undefined}>
+                    <NavLink to={'/liked'} className={({isActive}) => isActive ? classes.active : undefined}>
                         <svg width="20" height="20" viewBox="0 0 20 17.925" fill="none">
                             <path
                                 d="M9.29334 2.42109L10 3.12684L10.7067 2.42109C12.6039 0.526302 15.6804 0.526302 17.5776 2.42109C19.4741 4.31518 19.4741 7.38556 17.5776 9.27964L10 16.8476L2.42238 9.27964C0.525872 7.38556 0.525872 4.31518 2.42238 2.42109C4.3196 0.526302 7.39613 0.526302 9.29334 2.42109Z"
@@ -19,7 +18,7 @@ const Navigation = () => {
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to={'/search'} className={pathname === 'search' ? classes.active : undefined}>
+                    <NavLink to={'/search'} className={({isActive}) => isActive ? classes.active : undefined}>
                         <svg width="20" height="20" viewBox="-2 0 20 20" fill="none">
                             <path
                                 d="M17.0014 9.00071C17.0014 13.4194 13.4194 17.0014 9.00071 17.0014C4.58204 17.0014 1 13.4194 1 9.00071C1 4.58204 4.58204 1 9.00071 1C13.4194 1 17.0014 4.58204 17.0014 9.00071Z"
@@ -31,7 +30,7 @@ const Navigation = () => {
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to={'/123'} className={pathname === '/123' ? classes.active : undefined}>
+                    <NavLink to={'/123'} className={({isActive}) => isActive ? classes.active : undefined}>
                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
                             <path d="M10 1V10M10 10V19M10 10H1M10 10H19" strokeWidth="2"
                                   strokeLinecap="round"/>
