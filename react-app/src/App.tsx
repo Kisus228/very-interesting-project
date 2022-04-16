8a6b4252d336228c75b8459fd89ed503d7fa4da2
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-04-16 16:33:15 +0500

    Added ProfileMenu

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,10 +2,11 @@ import classes from './App.less';
 import React from 'react';
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Search from "./components/Search/Search";
 import Liked from "./components/Liked/Liked";
 import ProfileInfo from "./components/ProfileInfo/ProfileInfo";
+import MyProfile from "./components/MyProfile/MyProfile";
 
 function App() {
     return (
@@ -17,10 +18,11 @@ function App() {
                     <Routes>
                         <Route path="/">
                             <Route path="search">
-                                <Route path=":profileId" element={<ProfileInfo />}/>
-                                <Route index element={<Search />}/>
+                                <Route path=":profileId" element={<ProfileInfo/>}/>
+                                <Route index element={<Search/>}/>
                             </Route>
-                            <Route path="liked" element={<Liked />}/>
+                            <Route path="liked" element={<Liked/>}/>
+                            <Route path="profile" element={<MyProfile/>}/>
                         </Route>
                     </Routes>
                 </div>
--- react-app/src/components/Header/Header.tsx
+++ react-app/src/components/Header/Header.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./Header.less";
+import cn from "classnames";
 import logo from './../../assets/logo.png';
 import avatar from './../../assets/avatar.png';
+import {Link, useNavigate} from "react-router-dom";
 
 // TODO: Сергей Кашкин | Верстка: Сделать отцентровку изображения в квадрат без сжатия.
-// TODO: Сергей Кашкин | Верстка: Добавить выпадашку при нажатии на кнопку профиля.
 // TODO: Сергей Кашкин | Верстка: Подумать над шрифтами.
 // TODO: Сергей Кашкин | Верстка: Добавить медиа запросы для адаптива.
 
 const Header = () => {
+    const [openedProfileMenu, setOpenedProfileMenu] = useState(false);
+    const navigate = useNavigate();
+
+    const onClickProfileInfo = () => {
+        setOpenedProfileMenu(true);
+    }
+
+    const onBlurProfileInfo = () => {
+        setOpenedProfileMenu(false);
+    }
+
+    const onClickProfileMenu = (e: React.SyntheticEvent) => {
+        e.stopPropagation();
+        setOpenedProfileMenu(false);
+        navigate("profile");
+    }
+
     return (
         <header className={classes.Header}>
             <div className={classes.Logo}>
                 <img width={110} height={28} src={logo} alt={"logo"}/>
             </div>
             <div className={classes.Menu}>
-                <button className={classes.Button}>
+                <button className={classes.Button} onClick={onClickProfileInfo} onBlur={onBlurProfileInfo}>
                     <div className={classes.ProfileInfo}>
                         <p>Сергей Сергеич</p>
                         <p className={classes.Description}>Проводник, фронтовик, сосочка</p>
@@ -29,6 +47,19 @@ const Header = () => {
                                   strokeLinejoin="round"/>
                         </svg>
                     </div>
+                    {
+                        openedProfileMenu &&
+                        <div className={cn(classes.ProfileMenuWrapper)}>
+                            <div onClick={onClickProfileMenu} className={classes.ProfileMenuContent}>
+                                <svg width="20" height="20" viewBox="0 0 25 30" fill="none">
+                                    <path
+                                        d="M8.07692 8H17.8077M8.07692 22H17.8077M8.07692 15H16.0385M23 1H2C1.44772 1 1 1.44771 1 2V28C1 28.5523 1.44772 29 2 29H23C23.5523 29 24 28.5523 24 28V2C24 1.44771 23.5523 1 23 1Z"
+                                        stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                </svg>
+                                <span>Мой профиль</span>
+                            </div>
+                        </div>
+                    }
                 </button>
                 <div className={classes.Stroke}/>
                 <button className={classes.Button}>
