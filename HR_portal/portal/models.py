28fb7f0515694a2ca14f35f13f6de93f30eb1749
sukhr <sukhrab2001@mail.ru>
2022-04-17 21:32:09 +0500

    таблицы Резюме, Работник, Заявки на вакансии

--- HR_portal/portal/models.py
+++ HR_portal/portal/models.py
@@ -65,39 +65,35 @@ class Vacancy(models.Model):
         return self.name
 
 
+class Resume(models.Model):     # возможно ссылки передавать одним джейсон стетхэмом файлом вида: {'соцсеть': 'ссылка'}
+    vk_link = models.TextField(verbose_name='Ссылка на ВК')
+    tg_link = models.TextField(verbose_name='Ссылка на Телеграм')
+    github_link = models.TextField(verbose_name='Ссылка на GitHub')
+    gitlab_link = models.TextField(verbose_name='Ссылка на GitLab')
+    resume_text = models.TextField(verbose_name='Текст резюме')
+    skills = models.ManyToManyField(Skills, verbose_name='Список навыков')
+
+    class Meta:
+        verbose_name = 'Резюме'
+        verbose_name_plural = 'Резюме'
+
+
 class Worker(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Работник', blank=True)
-    birthdate = models.DateField(verbose_name='Дата рождения', blank=True)
-    liked_apps = models.JSONField(verbose_name='Понравившиеся заявки', blank=True)
+    liked_apps = models.ManyToManyField(Vacancy, verbose_name='Понравившиеся заявки', blank=True)
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE, verbose_name='Резюме')
 
     def __str__(self):
         return str(self.user)
 
 
 class JobApplications(models.Model):
-    vacancy_id = models.ForeignKey(Vacancy, on_delete=models.CASCADE, verbose_name='ID вакансии')
-    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE, verbose_name='ID работника')
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, verbose_name='ID вакансии')
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE, verbose_name='ID работника')
 
     class Meta:
         verbose_name = 'Заявка на вакансию'
         verbose_name_plural = 'Заявки на вакансии'
 
     def __str__(self):
-        return self.vacancy_id
-
-
-class Resume(models.Model):     # возможно ссылки передавать одним джейсон стетхэмом файлом вида: {'соцсеть': 'ссылка'}
-    resume_id = models.IntegerField(verbose_name='ID резюме')
-    vk_link = models.TextField(verbose_name='Ссылка на ВК')
-    tg_link = models.TextField(verbose_name='Ссылка на Телеграм')
-    github_link = models.TextField(verbose_name='Ссылка на GitHub')
-    gitlab_link = models.TextField(verbose_name='Ссылка на GitLab')
-    resume_text = models.TextField(verbose_name='Текст резюме')
-    skills = models.JSONField(verbose_name='Список навыков')
-    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE, verbose_name='ID работника')
-
-    class Meta:
-        verbose_name = 'Резюме'
-
-    def __str__(self):
-        return self.resume_id
+        return self.vacancy
