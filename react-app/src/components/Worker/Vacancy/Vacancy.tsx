9f6229a33774e65cbd4926117a01b0594173795d
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-05-18 21:55:24 +0500

    Mobile adaptive

--- react-app/src/components/Worker/Vacancy/Vacancy.tsx
+++ react-app/src/components/Worker/Vacancy/Vacancy.tsx
@@ -31,6 +31,13 @@ const Vacancy: React.FC<Props> = (props) => {
                             <h3>{vacancy.name}</h3>
                             <p className={classes.Description}>{vacancy.salary}</p>
                         </div>
+                    </section>
+                    <section className={classes.VacancySection}>
+                        <div className={classes.LikeButton}>
+                            <LikeButton liked={vacancy.is_liked} onClick={() => likeVacancyTC(vacancy.id, true)}/>
+                        </div>
+                    </section>
+                    <section className={classes.VacancySection}>
                         <div>
                             <h4>Специальность: {vacancy.specialization}</h4>
                             <p className={classes.Description}>Количество мест: {vacancy.count}</p>
@@ -51,9 +58,6 @@ const Vacancy: React.FC<Props> = (props) => {
                         </div>
                     </section>
                     <section className={classes.VacancySection}>
-                        <div className={classes.LikeButton}>
-                            <LikeButton liked={vacancy.is_liked} onClick={() => likeVacancyTC(vacancy.id, true)}/>
-                        </div>
                         <div className={classes.ProfileAvatarWrapper}>
                             <div className={classes.ProfileAvatarLarge}>
                                 <img width={150} height={150} src={avatar} alt={"avatar"}/>
